073a1447f3638f16b7885012aebb6793b80c5163
philip ka sing leung <philip.leung90@gmail.com>
2017-12-09 19:57:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(obj, key, value ){
-  var newVal = {key : value};
+  var newVal = {[key] : value};
   
   return Object.assign({} , obj , newVal);
 }
